87b75e713a80db0e72a40f2163723daf6e2185e3
0rakul0 <0rakul0render@gmail.com>
2020-10-18 00:30:56 -0300

    final
    falta revisar.. os icones não aparecem ...

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,19 +1,24 @@
-import React from 'react';
 
-import {useFonts} from 'expo-font';
-import {Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold} from '@expo-google-fonts/nunito';
+import React from 'react';
+import { useFonts } from 'expo-font';
+import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from '@expo-google-fonts/nunito';
+
 import Routes from './src/routes';
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    Nunito_600SemiBold, 
+
+  const [ fontsLoaded ] = useFonts({
+    Nunito_600SemiBold,
     Nunito_700Bold,
     Nunito_800ExtraBold
-  });
-  if (!fontsLoaded){
+  })
+
+  if (!fontsLoaded) {
     return null;
   }
+
   return (
-   <Routes />
+    <Routes />
   )
+
 }
--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -1,36 +1,25 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native'
-import {createStackNavigator} from '@react-navigation/stack'
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-const {Screen, Navigator} = createStackNavigator();
-import OrphanagesMap from './pages/OrphanagesMap'
-import OrphanagesDetails from './pages/OrphanageDetails';
-import OrphanageData from './pages/CreateOphanage/OrphanageData';
+
+const { Navigator, Screen } = createStackNavigator();
+
+import OrphanagesMap from './pages/OrphanagesMap';
+import OrphanageDetails from './pages/OrphanageDetails';
 import SelectMapPosition from './pages/CreateOphanage/SelectMapPosition';
+import OrphanageData from './pages/CreateOphanage/OrphanageData'
 import Header from './components/Header';
 
-
-
-export default function Routes(){
+export default function Routes() {
     return (
         <NavigationContainer>
-            <Navigator screenOptions={{headerShown:false, cardStyle:{backgroundColor:'#f2f3f5'}}}>
-                <Screen name="OrphanagesMap" component={OrphanagesMap}/>
-                <Screen name="OrphanagesDetails" component={OrphanagesDetails} 
-                options={{
-                    headerShown: true,
-                    header:()=><Header title="Orfanato"/>
-                }}/>
-                <Screen name="SelectMapPosition" component={SelectMapPosition}
-                options={{
-                    headerShown: true,
-                    header:()=><Header showCancel={false} title="Selecione no mapa"/>
-                }}/>
-                <Screen name="OrphanagesData" component={OrphanageData}
-                options={{
-                    headerShown: true,
-                    header:()=><Header title="Informe os dados"/>
-                }}/>
+            <Navigator screenOptions={{ headerShown: false, cardStyle: { backgroundColor: "#f1f3f5" }}} >
+                <Screen name='OrphanagesMap' component={OrphanagesMap} />
+                <Screen name='OrphanageDetails' component={OrphanageDetails} options={{ headerShown: true, header: () => <Header showCancel={false} title='Orfanato' /> }} />
+                <Screen name='SelectMapPosition' component={SelectMapPosition} options={{ headerShown: true, header: () => <Header title='Selecione no mapa' /> }} />
+                <Screen name='OrphanageData' component={OrphanageData} options={{ headerShown: true, header: () => <Header title='Informe os dados' /> }} />
             </Navigator>
         </NavigationContainer>
     )
